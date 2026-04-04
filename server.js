680c448f8135d9ec489a24a11dad949f470f0f6e
atharva318 <71706322+atharva318@users.noreply.github.com>
2026-04-04 19:17:32 +0530

    addedL

--- server.js
+++ server.js
@@ -166,5 +166,5 @@ app.post("/api/predict", (req, res) => {
 });
 
 /* ===== START SERVER ===== */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
